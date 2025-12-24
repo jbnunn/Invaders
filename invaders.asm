aa0c605afae63d1599b5cfa62fc53195627561b9
Jeff Nunn <jbnunn@gmail.com>
2025-12-24 07:27:05 -0600

    Clarified cmp in swarm check

--- invaders.asm
+++ invaders.asm
@@ -184,8 +184,7 @@ restart_game:
     ; ------------------
     mov ax, 0x08 * ROW_STRIDE + 0x28            ; Calculates the initial memory offset for the first invader. This is equivalent to
                                                 ; Y pos = 8 big rows down, X pos = 40 (28 hex = 40 dec)
-    mov bx, START_COLOR * 0x0100 + 0x10         ; Loads the color of the invader to BH, and a type for the invader in BL. Invaders have 
-                                                ; ??? different sprites
+    mov bx, START_COLOR * 0x0100 + 0x10         ; Loads the color of the invader to BH, and a type for the invader in BL. 
     mov dh, 0x05                                ; Initialize the outer loop counter for 5 rows of invaders. In the original code, the 
                                                 ; author had used a hacky comparison against the length of the color list in order
                                                 ; to save some bytes, but I found it very undreadable and confusing. This line was
@@ -278,10 +277,10 @@ draw_invader:
     call draw_sprite                                ; Draw the invader using the type/color in AX and position in DI 
 
 move_invader_swarm:   
-    cmp si, sprites + 56 * SPRITE_SIZE              ; SI is a pointer that iterates through every invader in th sprites table. We start at sprites + SPRITE_SIZE
-                                                    ; and we need to calculate the memory address immediately after the 55th invader. 
-                                                    ; If we've finished processing all 55 invaders for this frame... 
-    jne check_invader_state                         ; ... then we jump back to check_invader_state to process the next invader.
+    cmp si, sprites + 56 * SPRITE_SIZE              ; Check to see if we processed all 55 invaders. SI starts at 'sprites + 4' and increments by 4 for each invader.
+                                                    ; 56 * SPRITE_SIZE (224 bytes) is the memory address exactly after the last invader. 'cmp' performs an implicit 
+                                                    ; subtraction (SI - address). If the result is 0, the zero flag (ZF) is 1, otherwise ZF is set to 0. 
+    jne check_invader_state                         ; If ZF is 0 (not equal), it jumps back to process the next invader.
 
     ; We've processed all invaders on the row. Let's see what direction the swarm should be moving now
     mov al, dh                                      ; Take the next state value we put in DH and move it to Al
@@ -317,7 +316,7 @@ animate_invader:
     mov bp, 0                                       ; If yes, reset counter to 0
 
     .skip_reset:                                    
-      pusha                                         ; Save registers -- ??? but why
+      pusha                                         ; Save registers
 
       cmp bp, 0                                     ; Is the counter at 0? (Meaning we just hit the 8th invader)
       jne handle_player_bullet                      ; If BP is NOT 0, skip the wait and jump to handle_player_bullet
